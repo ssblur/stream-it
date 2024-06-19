299f3d63878eeaf37056e651368a98dc8abfa47d
Patrick Hallbick <patrick.hallbick@outlook.com>
2024-06-18 21:21:54 -0400

    Wait no go back

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 pipenv run pyinstaller --onefile --clean -y ^
  --add-data "src/modules/:src/modules/" ^
- --noconsole ^
+ --hide-console hide-early ^
  --icon "src/client/favicon.ico" ^
  --add-data "src/client/*:src/client/" ^
  -n "StreamIt" ^
