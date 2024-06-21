af2053a4a7d81d5e52520369ac3733fecb393239
Patrick Hallbick <patrick.hallbick@outlook.com>
2024-06-21 17:45:46 -0400

    Add system tray icon on Windows

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -1,7 +1,32 @@
 from http.server import ThreadingHTTPServer
 from .handler import Handler
 from socket import gethostbyname, gethostname
+from sys import platform
+from ..util import module_path
+import webbrowser
 
 def start(addr, port):
     server = ThreadingHTTPServer((addr, port), Handler)
+
+    if platform == "win32":
+        from infi.systray import SysTrayIcon
+
+        tray = SysTrayIcon(
+            module_path("./src/client/favicon.ico"),
+            "Stream It!",
+            (
+                (
+                    "Open Manage Panel", 
+                    None, 
+                    lambda: webbrowser.open(
+                        f"http://{server.server_address[0]}{server.server_address[1]}:/manage", 
+                        new=0, 
+                        autoraise=True
+                    )
+                ),
+            ),
+            on_quit=lambda t: server.shutdown()
+        )
+        tray.start()
+
     server.serve_forever()
--- src/util.py
+++ src/util.py
@@ -5,6 +5,9 @@ def frozen():
     return getattr(sys, 'frozen', False)
 
 def module_file(path, flags="rb"):
+    return open(module_path(path), flags)
+
+def module_path(path):
     if getattr(sys, 'frozen', False):
-        path = join(sys._MEIPASS, path)
-    return open(path, flags)
+        return join(sys._MEIPASS, path)
+    return path
